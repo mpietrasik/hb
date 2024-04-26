5f5d6111f331300dfbc8ceef5db8fa1fcdf32be3
Pietrasik <P70087271@unimaas.nl>
2024-04-26 15:01:51 +0200

    compiler added

---
+++ compile.m
@@ -0,0 +1,11 @@
+% Compiles sampler using Boost 1.67.0 C++ libraries
+%
+% If compiling for 64-bit MATLAB, boost should be compiled 
+% with "bjam address-model=64" to produce 64-bit libraries.
+
+BOOST_PATH      = 'C:/Users/marci/boost_1_67_0_old_pc';            
+BOOST_LIB_PATH  = 'C:/Users/marci/boost_1_67_0_old_pc/stage/lib'; 
+
+eval(['mex COMPFLAGS="$COMPFLAGS -I' BOOST_PATH '" LINKFLAGS="$LINKFLAGS /LIBPATH:' BOOST_LIB_PATH '" sampler.cpp'])
+
+clear BOOST_PATH BOOST_LIB_PATH;
